b799663bae960bd36db2a97eceb0ebaf6d753a2c
bhaumik2784 <119956070+bhaumik2784@users.noreply.github.com>
2023-09-24 17:19:46 +0530

    ....okkk

---
+++ src/data.js
@@ -0,0 +1,225 @@
+export const data = data = [ {
+    "name": "Green Waves Environmental Solutions",
+    "latitude": "17.70353003603317",
+    "longitude": "83.21149876008126",
+    "contact": "07093351666",
+    "website": "https://www.greenwaveindia.com/"
+},
+{
+    "name": "Apna Bhoomi Ewaste Management Services",
+    "latitude": "18.763286842580047",
+    "longitude": "84.06121144919295",
+    "contact": "9515151339",
+    "website": "http://apnabhoomi.com/"
+},
+{
+    "name": "Veera Waste Management Systems",
+    "latitude": "17.701567682529586",
+    "longitude": "83.18593349391263",
+    "contact": "07569309142",
+    "website": "http://apnabhoomi.com/"
+},
+{
+    "name": "UNITED GLOBAL TRUST",
+    "latitude": "26.1831445134194",
+    "longitude": "91.77507091734641",
+    "contact": "09330089998",
+    "website": "https://datagemba.com/b/v/in/guwahati/rubbish-removal/ChIJt_ZlmhxcWjcR5U3nY5-XzXc"
+},
+{
+    "name": "Veera Waste Management Systems",
+    "latitude": "17.701567682529586",
+    "longitude": "83.18593349391263",
+    "contact": "9515151339",
+    "website": "http://apnabhoomi.com/"
+},
+{
+    "name": "ADV METAL COMBINE PRIVATE LIMITED",
+    "latitude": "28.629057674177304",
+    "longitude": "77.08272670185448",
+    "contact": "09999798939",
+    "website": "https://advindia.com/"
+},
+{
+    "name": "Star E Processors",
+    "latitude": "21.194953565742175",
+    "longitude": "81.96986867966356",
+    "contact": "8047314824",
+    "website": "https://stareprocessors.com/Hazardous-waste-Management.html"
+},
+{
+    "name": "Fozia Traders",
+    "latitude": "28.675881468097035",
+    "longitude": "77.26012328891538",
+    "contact": "09810635195",
+    "website": "http://foziatraders.in/"
+},
+{
+    "name": "Muskan Technologies",
+    "latitude": "28.523008276186562",
+    "longitude": "77.28311765115086",
+    "contact": "9899888815",
+    "website": "https://www.justdial.com/jdmart/Delhi/Muskan-Technologies-Okhla-Industrial-Area-Phase-1/011PXX11-XX11-181207210914-Y5N6_BZDET/catalogue"
+},
+{
+    "name": "Namo eWaste Management Ltd.",
+    "latitude": "28.48318996298598",
+    "longitude": "77.32026557565875",
+    "contact": "08130393629",
+    "website": "http://www.namoewaste.com/"
+},
+{
+    "name": "E-coli waste management pvt ltd",
+    "latitude": "23.653349837508586",
+    "longitude": "73.31342779625517",
+    "contact": "09898043850",
+    "website": "https://ecoliwaste.com/"
+},
+{
+    "name": "Earth Waste Management Company",
+    "latitude": "28.56285895388797",
+    "longitude": "77.2493676019312",
+    "contact": "09312702965",
+    "website": "http://www.eemplindia.com/companyprofile.html"
+},
+{
+    "name": "Basant Clean Enviro Ltd.",
+    "latitude": "23.00627926898927",
+    "longitude": "72.50252571727272",
+    "contact": "07929708079",
+    "website": "http://bcel.in/"
+},
+{
+    "name": "Ecotime Industries",
+    "latitude": "21.037825287652634",
+    "longitude": "72.9287370577543",
+    "contact": "09913117758",
+    "website": "http://www.eco-time.in/"
+},
+{
+    "name": "Tvarita Phones Pvt Ltd",
+    "latitude": "20.355113642024957",
+    "longitude": "72.90967596699262",
+    "contact": "02262456666",
+    "website": "https://tvarita.global/"
+},
+{
+    "name": "Evergreen Recyclekaro India Private Limited",
+    "latitude": "19.688147825773516",
+    "longitude": "73.12890472887327",
+    "contact": "18003099308",
+    "website": "http://www.recyclekaro.com/"
+},
+{
+    "name": "Egnus Ewaste Solution Private Limited.",
+    "latitude": "21.230050116566005",
+    "longitude": "72.82153124894313",
+    "contact": "09737471912",
+    "website": "http://egnus.in/"
+},
+{
+    "name": "Global E-Waste Management System",
+    "latitude": "15.271116112254282",
+    "longitude": "74.00092211292227",
+    "contact": "07798001629",
+    "website": "http://www.gemsrecyclers.com/"
+},
+{
+    "name": "Exigo Recycling Pvt. Ltd.",
+    "latitude": "29.46210138390716",
+    "longitude": "77.01916750099343",
+    "contact": "09650676997",
+    "website": "http://www.exigorecycling.com/"
+},
+{
+    "name": "TES-AMM India Private Limited",
+    "latitude": "28.916956403389552",
+    "longitude": "77.08853090577499",
+    "contact": "18001208875",
+    "website": "https://tes-amm.net/"
+},
+{
+    "name": "Deshwal Waste Management Pvt Ltd",
+    "latitude": "28.38535964595176",
+    "longitude": "76.8829357150118",
+    "contact": "18001029077",
+    "website": "https://www.dwmpl.com/"
+},
+{
+    "name": "Pegasus - eWaste Management Company",
+    "latitude": "28.348950707588546",
+    "longitude": "77.08355218237237",
+    "contact": "09818346370",
+    "website": "http://thepegasus.co.in/"
+},
+{
+    "name": "Shivalik Solid Waste Management Ltd.",
+    "latitude": "30.677047578750297",
+    "longitude": "76.82878352861907",
+    "contact": "01795260227",
+    "website": "http://www.sswml.com/"
+},
+{
+    "name": "Cerebra Integrated Technologies Limited",
+    "latitude": "13.034189324223393",
+    "longitude": "77.52064219958832",
+    "contact": "180042546969",
+    "website": "http://www.cerebracomputers.com/"
+},
+{
+    "name": "E-Parisaraa Pvt. Ltd",
+    "latitude": "12.910886884669901",
+    "longitude": "77.60446202500246",
+    "contact": "09980147680",
+    "website": "https://ewasteindia.com/"
+},
+{
+    "name": "Recycler India MGT LLP",
+    "latitude": "12.917858437873173",
+    "longitude": "77.45851050052721",
+    "contact": "18004194937",
+    "website": "http://recyclerindiamgt.com/"
+},
+{
+    "name": "Kerala Enviro Infrastructure Limited",
+    "latitude": "9.98232282910488",
+    "longitude": "76.3636309963882",
+    "contact": "04842722141",
+    "website": "http://keralaenviro.com/"
+},
+{
+    "name": "Evergreen Recyclekaro india pvt ltd",
+    "latitude": "19.72675129861352",
+    "longitude": "73.07729799366552",
+    "contact": "99673 10007",
+    "website": "https://www.recyclekaro.com/"
+},
+{
+    "name": "Techeco Waste Management LLP",
+    "latitude": "19.008412851196983",
+    "longitude": "72.84454884652042",
+    "contact": "18008893121",
+    "website": "http://techeco.co.in/"
+},
+{
+    "name": "Ecoprogetti Production Process India Pvt Ltd.",
+    "latitude": "19.221004992917774",
+    "longitude": "72.90246900818859",
+    "contact": "09820068331",
+    "website": "http://www.ecoprogetti.com/"
+},
+{
+    "name": "Unique Eco Recycle",
+    "latitude": "22.684334173599623",
+    "longitude": "75.9035610252236",
+    "contact": "08120888898",
+    "website": "http://www.uerindia.com/"
+},
+{
+    "name": "Unique Eco Recycle",
+    "latitude": "22.684334173599623",
+    "longitude": "75.9035610252236",
+    "contact": "08120888898",
+    "website": "http://www.uerindia.com/"
+},
+]
